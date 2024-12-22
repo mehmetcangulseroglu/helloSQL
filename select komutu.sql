da6df94904854159a5a7aa24f99785cc3e328290
mehmetcangulseroglu <mcagul99@gmail.com>
2024-12-22 20:33:14 +0300

    select islendi

---
+++ select komutu.sql
@@ -0,0 +1,9 @@
+--sql dml komutlari...
+--select,insert,update,delete,truncate
+
+--SELECT=tablolardan kayitlari çeker listeler.
+--select * from tableName where<sartlar>
+--select [sütun isimleri] from tableName where<sartlar>
+
+SELECT * FROM CUSTOMERS
+SELECT ID,NAMESURNAME,GENDER FROM CUSTOMERS

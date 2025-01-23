53fd90cf4d04603424ed2c24159d6d7898715417
mehmetcangulseroglu <mcagul99@gmail.com>
2025-01-23 15:43:17 +0300

    update islendi

---
+++ update komutu.sql
@@ -0,0 +1,6 @@
+--update komutu = bir veya birden fazla deðeri tabloda günceller.
+--update tableName set kolon1=deger1,kolon2=deger2.....(update syntax)
+
+
+--ÖRN:
+--UPDATE CUSTOMERS SET GENDER='E',CITY='MARDÝN'

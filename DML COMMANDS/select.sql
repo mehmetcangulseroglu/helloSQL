aeb865273cea5a272c9a2d02f59fc354749fca7d
mehmetcangulseroglu <mcagul99@gmail.com>
2025-07-19 19:22:30 +0300

    dml commands completed

---
+++ DML COMMANDS/select.sql
@@ -0,0 +1,13 @@
+--SELECT 
+
+--select kolon1,kolon2...... from tabloadi where<sartlar>
+
+SELECT ID, TCNUMBER, NAMESURNAME, GENDER, EMAÝL, BIRTHDATE, CITY, DISTRICT, TELNR1, TELNR2
+FROM CUSTOMERS
+
+--SELECT * FROM CUSTOMERS
+
+SELECT [ID],[TCNUMBER],[NAMESURNAME],[GENDER],[EMAÝL],[BIRTHDATE],[CITY],[DISTRICT],[TELNR1],[TELNR2]
+FROM CUSTOMERS-- bu da doðru sütun isimleri arasýnda boþluk olabilir name surname diye mesela bir bütün olarak algilansin diye bu yapilabilir
+
+	

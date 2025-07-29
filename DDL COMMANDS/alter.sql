c6fee57ae84f07c5cad8cd87e4dd088214b06019
mehmetcangulseroglu <mcagul99@gmail.com>
2025-07-29 21:47:53 +0300

    alter completed

---
+++ DDL COMMANDS/alter.sql
@@ -0,0 +1,15 @@
+--alter
+--mevcut tabloyu deðiþtirmeye veya modifiye etmeye yarar
+
+--alter table tabloadý add column -> tablomuza yeni bir sütun ekler
+--alter table tabloadý drop column -> tablomuzdan mevcut bir sütunu siler
+--alter table tabloadý alter column -> tablomuzdaki mevcut sütunu deðiþtirir varchar(1) dir varchar(15) yaparýz mesela
+
+--alter table tabloadý add constraýnt -> prýmary veya foreign key ekler 
+--alter table tabloadý drop constraýnt -> prýmary veya foreign key kaldýrýr
+
+--ALTER TABLE CUSTOMERS ADD COUNTRY VARCHAR(50) , AGE INT ...
+--ALTER TABLE CUSTOMERS DROP COLUMN COUNTRY
+
+--ALTER TABLE CUSTOMERS ALTER COLUMN GENDER VARCHAR(10)
+
